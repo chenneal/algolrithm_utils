18f9b1bc7ec2cd0d322d3e2845abd9cadc272e2d
chenneal <chenneal@126.com>
2016-10-25 00:19:03 +0800

    所有题目的头文件模板，不断更新

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <climits>
+#include <cctype>
+
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+
+#define pb push_back
+//#define FILE_JUDGE
+
+using namespace std;
+
+typedef long long ll;
+typedef double D;
+typedef vector<int> VI;
+typedef map<int, int> MII;
+typedef set<int> SI;
+
+int main(int argc, char* argv[]) {
+    #ifdef FILE_JUDGE
+    freopen("file.in", "r", stdin);
+    freopen("file.out", "w", stdout);
+    #endif
+    return 0;
+}
